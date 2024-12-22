bf8e7865bbc1b04cecdcb0fa0ac2391e88a0e288
ytjdud <102743371+ytjdud@users.noreply.github.com>
2024-12-22 13:06:51 +0900

    refactor: 데이터 모듈화

---
+++ public/enum.js
@@ -0,0 +1,14 @@
+export const ExternalLinks = Object.freeze({
+  "신강 10층": "https://store.nowwaiting.co/spots/14406",
+  "신강 7층": "https://store.nowwaiting.co/spots/15533",
+  "어퍼하우스": "https://store.nowwaiting.co/spots/18431",
+  "본점 13층": "https://store.nowwaiting.co/spots/15824",
+  "타임스퀘어": "https://store.nowwaiting.co/spots/16982",
+  "사우스시티": "https://store.nowwaiting.co/spots/18136"
+})
+
+export const ContactNumbers = Object.freeze({
+  "신강 7층": "02-3479-1020",
+  "본점 13층": "02-310-1020",
+  "타임스퀘어": "02-2639-1060"
+})
